104f3123a450a277d714f0cfaef1b868c536a4fd
PallaviNarkhede29 <pallavinarkhede182@gmail.com>
2025-04-05 10:11:36 +0530

    changes

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -9,6 +9,7 @@ let Home = () => {
 
   return (
     <>
+    <h1>welcome</h1>
       <div className="main-Home">
         <nav className="navbar navbar-expand-lg navbar-dark static-top shadow p-3 rounded sticky-top">
           <div className="container">
@@ -21,7 +22,7 @@ let Home = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="#" onClick={() => navigate("/")}>Home</a>
+                  <a className="nav-link" aria-current="page" href="#" >Home</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="" onClick={() => navigate("/about")}>About</a>
