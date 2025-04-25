fa289cfd9af4eff39b6f745f48b3ee5b10a5fd90
aishwaryaPangale <aishwaryapangale0@gmail.com>
2025-04-25 16:30:45 +0530

    changes

--- src/AdminDashBoard/AdminDashboard.jsx
+++ src/AdminDashBoard/AdminDashboard.jsx
@@ -5,8 +5,11 @@ import {
   FaHome, FaBook, FaClipboardList, FaUserGraduate,
   FaChartBar, FaSignOutAlt
 } from "react-icons/fa";
+import { FaUserShield } from "react-icons/fa";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const AdminDashboard = () => {
   const location = useLocation();
@@ -78,7 +81,8 @@ const AdminDashboard = () => {
       {/* Header */}
       <header className="bg-dark text-light d-flex justify-content-between align-items-center px-4 py-2 shadow">
         <h4 className="m-0">Admin Dashboard</h4>
-        <span><strong>{username || "Admin"}</strong></span>
+        
+       <FontAwesomeIcon icon={faUser} style={{marginLeft:"830px"}}></FontAwesomeIcon><strong>{username || "Admin"}</strong>
       </header>
 
       {/* Layout */}
